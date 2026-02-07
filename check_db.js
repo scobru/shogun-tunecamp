ee6f96b8de017468edcb121e0e96191f0678a8bb
Francesco Bruno <scobru1988@gmail.com>
2026-02-07 16:22:32 +0100

    fix upload

--- check_db.js
+++
@@ -1,6 +0,0 @@
-
-import Database from 'better-sqlite3';
-const db = new Database('tunecamp.db');
-
-const admins = db.prepare('SELECT id, username, artist_id FROM admin').all();
-console.log('Admins:', admins);
--- check_tables.js
+++
@@ -1,4 +0,0 @@
-import Database from 'better-sqlite3';
-const db = new Database('tunecamp.db');
-const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all();
-console.log('Tables:', tables.map(t => t.name).join(', '));
