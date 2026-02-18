ebb726278cb18fdd81f3cc7316a9595f9d20240c
Francesco Bruno <scobru1988@gmail.com>
2026-02-18 21:22:26 +0100

    new ui

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,65 +1,84 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { MainLayout } from './components/layout/MainLayout';
-import { Home, Albums, AlbumDetails, Artists, ArtistDetails, Tracks, Stats, Search, Network, Support, Playlists, PlaylistDetails, Post, AuthCallback } from './pages';
-import Admin from './pages/Admin';
-import AdminReleaseEditor from './pages/AdminReleaseEditor';
-import Files from './pages/Files';
-import { useAuthStore } from './stores/useAuthStore';
-import { useEffect } from 'react';
-import { ForcePasswordChangeModal } from './components/modals/ForcePasswordChangeModal';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { MainLayout } from "./components/layout/MainLayout";
+import {
+  Home,
+  Albums,
+  AlbumDetails,
+  Artists,
+  ArtistDetails,
+  Tracks,
+  Stats,
+  Search,
+  Network,
+  Support,
+  Playlists,
+  PlaylistDetails,
+  Post,
+  AuthCallback,
+} from "./pages";
+import Admin from "./pages/Admin";
+import AdminReleaseEditor from "./pages/AdminReleaseEditor";
+import Files from "./pages/Files";
+import { useAuthStore } from "./stores/useAuthStore";
+import { useEffect } from "react";
+import { ForcePasswordChangeModal } from "./components/modals/ForcePasswordChangeModal";
 
 function App() {
   const { init, checkAdminAuth } = useAuthStore();
 
   useEffect(() => {
     init();
-    
+
     const handleUnauthorized = () => {
-        checkAdminAuth(); // Re-check admin auth on 401
+      checkAdminAuth(); // Re-check admin auth on 401
     };
-    
-    window.addEventListener('auth:unauthorized', handleUnauthorized);
-    return () => window.removeEventListener('auth:unauthorized', handleUnauthorized);
+
+    window.addEventListener("auth:unauthorized", handleUnauthorized);
+    return () =>
+      window.removeEventListener("auth:unauthorized", handleUnauthorized);
   }, []);
 
   return (
     <>
-    <ForcePasswordChangeModal />
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        
-        {/* Library */}
-        <Route path="/albums" element={<Albums />} />
-        <Route path="/albums/:idOrSlug" element={<AlbumDetails />} />
-        <Route path="/artists" element={<Artists />} />
-        <Route path="/artists/:idOrSlug" element={<ArtistDetails />} />
-        <Route path="/tracks" element={<Tracks />} />
-        
-        {/* Features */}
-        <Route path="/search" element={<Search />} />
-        <Route path="/playlists" element={<Playlists />} />
-        <Route path="/playlists/:id" element={<PlaylistDetails />} />
-        <Route path="/post/:slug" element={<Post />} />
-        <Route path="/network" element={<Network />} />
-        <Route path="/stats" element={<Stats />} />
-        
-        {/* Auth Callback */}
-        <Route path="/auth/callback" element={<AuthCallback />} />
+      <ForcePasswordChangeModal />
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
 
-        {/* Admin */}
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/admin/release/new" element={<AdminReleaseEditor />} />
-        <Route path="/admin/release/:id/edit" element={<AdminReleaseEditor />} />
-        <Route path="/browser" element={<Files />} />
-        
-        {/* Other */}
-        <Route path="/support" element={<Support />} />
+          {/* Library */}
+          <Route path="/albums" element={<Albums />} />
+          <Route path="/albums/:idOrSlug" element={<AlbumDetails />} />
+          <Route path="/artists" element={<Artists />} />
+          <Route path="/artists/:idOrSlug" element={<ArtistDetails />} />
+          <Route path="/tracks" element={<Tracks />} />
 
-        {/* Catch-all */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
-    </Routes>
+          {/* Features */}
+          <Route path="/search" element={<Search />} />
+          <Route path="/playlists" element={<Playlists />} />
+          <Route path="/playlists/:id" element={<PlaylistDetails />} />
+          <Route path="/post/:slug" element={<Post />} />
+          <Route path="/network" element={<Network />} />
+          <Route path="/stats" element={<Stats />} />
+
+          {/* Auth Callback */}
+          <Route path="/auth/callback" element={<AuthCallback />} />
+
+          {/* Admin */}
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/release/new" element={<AdminReleaseEditor />} />
+          <Route
+            path="/admin/release/:id/edit"
+            element={<AdminReleaseEditor />}
+          />
+          <Route path="/browser" element={<Files />} />
+
+          {/* Other */}
+          <Route path="/support" element={<Support />} />
+
+          {/* Catch-all */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
     </>
   );
 }
