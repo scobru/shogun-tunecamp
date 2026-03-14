899206e1ca4f9dbbd445d338a6a6f15241230266
Francesco Bruno <scobru1988@gmail.com>
2026-03-14 18:25:53 +0100

    fix build errors

--- src/server/database.ts
+++ src/server/database.ts
@@ -2532,6 +2532,6 @@ export function createDatabase(dbPath: string): DatabaseService {
             const rows = db.prepare("SELECT owner_id FROM album_ownership WHERE album_id = ?").all(albumId) as { owner_id: number }[];
             return rows.map(r => r.owner_id);
         },
-    };
+    } as DatabaseService;
 }
 
