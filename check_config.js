e69390fa95d10eda47fb120e6df0abe65b4c7768
Francesco Bruno <scobru1988@gmail.com>
2026-03-16 17:55:08 +0100

    cleanup

--- check_config.js
+++
@@ -1,6 +0,0 @@
-import { loadConfig } from "./dist/server/config.js";
-import path from "path";
-
-const config = loadConfig();
-console.log("Database path:", config.dbPath);
-console.log("Current working directory:", process.cwd());
